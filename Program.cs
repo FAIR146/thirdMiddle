c8e25f9fbacb575e7c8d0ad5dfed66f9a9cb9d8c
FAIR146 <81648490+FAIR146@users.noreply.github.com>
2021-04-08 22:37:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_therd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввпедите 3 числа, что бы узнать их среднее: ");
+            
+            float first = int.Parse(Console.ReadLine()); 
+            float second = int.Parse(Console.ReadLine()); 
+            float third = int.Parse(Console.ReadLine());
+
+            float firstAction = first / 3;
+            float secondAction = firstAction + second;
+            float thirdAcrion = secondAction + third;
+
+
+            Console.WriteLine(thirdAcrion);
+
+            Console.ReadKey();
+
+        }
+    }
+}
